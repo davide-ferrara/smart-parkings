55279cf0cc9395420c3d0197d12a13e047d516d4
davide-ferrara <davide98ferrara@gmail.com>
2024-12-12 20:49:24 +0100

    starting auth impl.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,6 +8,7 @@
     <nav>
         <x-navbar-link href="/" :active="request()->is('/')">Home</x-navbar-link>
         <x-navbar-link href="/login" :active="request()->is('login')">Login</x-navbar-link>
+        <x-navbar-link href="/register" :active="request()->is('register')">Register</x-navbar-link>
     </nav>
 
     {{$slot}}
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,18 +4,16 @@
         <title>Register</title>
     </head>
     <body>
-        <h1>Register page</h1>
-        <div>
-            <form action="/register">
-                <input type="text" name="name" id="name" placeholder="Name">
-                <input type="text" name="surname" id="surname" placeholder="Surname">
-                <input type="text" name="email" id="email" placeholder="Email">
-                <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Phone number">
-                <input type="text" name="gender" id="gender">
-                <input type="text" name="dob" id="dob">
-                <input type="password" name="password" id="password" placeholder="password">
-                <button id="loginBtn">Register</button>
-            </form>
-        </div>
+    <x-navbar>
+        <h1>Register Page</h1>
+
+        <form method="POST" action="">
+            <x-input type="text" id="fusername" placeholder="username"></x-input>
+            <x-input type="password" id="fpassword" placeholder="password"></x-input>
+            <x-button type="submit">Register</x-button>
+        </form>
+
+    </x-navbar>
+
     </body>
 </html>
